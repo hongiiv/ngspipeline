8c242c880d8c1c23f075bb92321d33fbb22c3269
hongiiv <hongiiv@gmail.com>
2015-05-11 16:04:19 +0900

    first commit
    first commit

---
+++ getdata.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#set -eu -o pipefail
+
+# Run workflow
+cd /bio
+DATE=`date +%Y-%m-%d`
+mkdir -p workflow-${DATE}/config workflow-${DATE}/input workflow-${DATE}/work
+cd workflow-${DATE}/config
+wget https://raw.githubusercontent.com/chapmanb/bcbio-nextgen/master/config/examples/cancer-dream-syn3.yaml
+
+rm -rf /usr/local/share/bcbio/genomes
+rm -rf /usr/local/share/bcbio/galaxy
+
+cd /usr/local/share/bcbio
+ln -s /bio/galaxy ./galaxy
+ln -s /bio/genomes ./genomes
+
+wget --no-check-certificate https://ssproxy.ucloudbiz.olleh.com/v1/AUTH_f1b97694-00cd-4e06-b9f3-30a0f9d01f66/bcbio/genome.py
+mv genome.py /usr/local/share/bcbio/anaconda/lib/python2.7/site-packages/bcbio/pipeline/genome.py
+
+# Exome only data
+cd /bio/workflow-${DATE}/input
+wget --no-check-certificate https://ssproxy.ucloudbiz.olleh.com/v1/AUTH_f1b97694-00cd-4e06-b9f3-30a0f9d01f66/bcbio/synthetic_challenge_set3_normal_NGv3_1.fq.gz
+wget --no-check-certificate https://ssproxy.ucloudbiz.olleh.com/v1/AUTH_f1b97694-00cd-4e06-b9f3-30a0f9d01f66/bcbio/synthetic_challenge_set3_normal_NGv3_2.fq.gz
+wget --no-check-certificate https://ssproxy.ucloudbiz.olleh.com/v1/AUTH_f1b97694-00cd-4e06-b9f3-30a0f9d01f66/bcbio/synthetic_challenge_set3_tumor_NGv3_1.fq.gz
+wget --no-check-certificate https://ssproxy.ucloudbiz.olleh.com/v1/AUTH_f1b97694-00cd-4e06-b9f3-30a0f9d01f66/bcbio/synthetic_challenge_set3_tumor_NGv3_2.fq.gz
+
+# Evaluation data and BED files
+wget https://s3.amazonaws.com/bcbio_nextgen/dream/synthetic_challenge_set3_tumor_20pctmasked_truth.tar.gz
+tar -xzvpf synthetic_challenge_set3_tumor_20pctmasked_truth.tar.gz
+wget -O refseq-merged.bed.gz https://s3.amazonaws.com/bcbio_nextgen/dream/refseq-merged.bed.gz
+wget -O NGv3.bed.gz https://s3.amazonaws.com/bcbio_nextgen/NGv3.bed.gz
+gunzip *.bed.gz
+
+cd /bio/workflow-${DATE}/work
+bcbio_nextgen.py ../config/cancer-dream-syn3.yaml -n 8
---
+++ prepare.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#set -eu -o pipefail
+
+# This script run at free disk space large then 20 GB
+# Full input data
+#mkdir -p /bio
+#cd /bio
+wget --no-check-certificat https://ssproxy.ucloudbiz.olleh.com/v1/AUTH_f1b97694-00cd-4e06-b9f3-30a0f9d01f66/bcbio/genomes.tar.gzaa
+wget --no-check-certificat https://ssproxy.ucloudbiz.olleh.com/v1/AUTH_f1b97694-00cd-4e06-b9f3-30a0f9d01f66/bcbio/genomes.tar.gzab
+wget --no-check-certificat https://ssproxy.ucloudbiz.olleh.com/v1/AUTH_f1b97694-00cd-4e06-b9f3-30a0f9d01f66/bcbio/genomes.tar.gzac
+wget --no-check-certificat https://ssproxy.ucloudbiz.olleh.com/v1/AUTH_f1b97694-00cd-4e06-b9f3-30a0f9d01f66/bcbio/genomes.tar.gzad
+wget --no-check-certificat https://ssproxy.ucloudbiz.olleh.com/v1/AUTH_f1b97694-00cd-4e06-b9f3-30a0f9d01f66/bcbio/genomes.tar.gzae
+wget --no-check-certificat https://ssproxy.ucloudbiz.olleh.com/v1/AUTH_f1b97694-00cd-4e06-b9f3-30a0f9d01f66/bcbio/genomes.tar.gzaf
+wget --no-check-certificat https://ssproxy.ucloudbiz.olleh.com/v1/AUTH_f1b97694-00cd-4e06-b9f3-30a0f9d01f66/bcbio/genomes.tar.gzag
+wget --no-check-certificat https://ssproxy.ucloudbiz.olleh.com/v1/AUTH_f1b97694-00cd-4e06-b9f3-30a0f9d01f66/bcbio/genomes.tar.gzah
+wget --no-check-certificat https://ssproxy.ucloudbiz.olleh.com/v1/AUTH_f1b97694-00cd-4e06-b9f3-30a0f9d01f66/bcbio/genomes.tar.gzai
+cat genomes.tar.gz* | tar xvfz -
+rm -rf galaxy.tar.gz genomes.bwa.tar.gz genomes.seq.tar.gz genomes.variation.tar.gz
+
+# Setup base system
+apt-get update -y && apt-get install linux-image-generic-lts-trusty -y
+wget --no-check-certificat -qO- https://get.docker.com/ | sh
+
+# Docker run
+docker pull hongiiv/bcbio:0.2
+docker run -t -i -v /bio:/bio hongiiv/bcbio:0.2 /bin/bash
+
+
+# Run workflow
+#DATE=`date +%Y-%m-%d`
+#mkdir -p workflow-${DATE}/config workflow-${DATE}/input workflow-${DATE}/work
+#cd workflow-${DATE}/config
+#wget https://raw.githubusercontent.com/chapmanb/bcbio-nextgen/master/config/examples/cancer-dream-syn3.yaml
+# Download raw fastq from any where
+#
+#cd ../work
+#bcbio_nextgen.py ../config/cancer-dream-syn3.yaml
